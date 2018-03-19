dcb5f454ef05c9e08940a96eb7acde76d80dbb04
kaylee <1126238932@qq.com>
2018-03-19 15:34:45 +0800

    优化，将服务改成了前台服务

--- app/src/main/java/com/example/kaylee/redenvelopapplication/RedEnvelopeService.java
+++ app/src/main/java/com/example/kaylee/redenvelopapplication/RedEnvelopeService.java
@@ -4,11 +4,17 @@ import android.accessibilityservice.AccessibilityService;
 import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Build;
+import android.support.annotation.IntDef;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
 import java.util.List;
+
+import static java.lang.Thread.sleep;
 
 /**
  * Created by kaylee on 2018/3/16.
@@ -25,7 +31,7 @@ public class RedEnvelopeService extends AccessibilityService {
             case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
             case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
                 String className = event.getClassName().toString();
-                if (className.equals("com.tencent.mm.ui.LauncherUI")) {
+                if (className.equals("com.tencent.mm.ui.LauncherUI") || className.equals("android.widget.ListView")) {
                     getPacket();
                 } else if (className.equals("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI")) {
                     openPacket();
@@ -38,7 +44,7 @@ public class RedEnvelopeService extends AccessibilityService {
 
     /**
      * 处理通知栏信息
-     *
+     * <p>
      * 如果是微信红包的提示信息,则模拟点击
      *
      * @param event
@@ -72,11 +78,18 @@ public class RedEnvelopeService extends AccessibilityService {
         AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
         if (nodeInfo != null) {
             //为了演示,直接查看了关闭按钮的id
-            List<AccessibilityNodeInfo> infos = nodeInfo.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/hy");
+            final List<AccessibilityNodeInfo> infos = nodeInfo.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/hx");
             nodeInfo.recycle();
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             for (AccessibilityNodeInfo item : infos) {
                 item.performAction(AccessibilityNodeInfo.ACTION_CLICK);
             }
+
+
         }
     }
 
@@ -103,22 +116,22 @@ public class RedEnvelopeService extends AccessibilityService {
     private void getPacket() {
         AccessibilityNodeInfo rootNode = getRootInActiveWindow();
         AccessibilityNodeInfo node = recycle(rootNode);
-
-        node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-        AccessibilityNodeInfo parent = node.getParent();
-        while (parent != null) {
-            if (parent.isClickable()) {
-                parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                break;
+        if (node != null) {
+            node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            AccessibilityNodeInfo parent = node.getParent();
+            while (parent != null) {
+                if (parent.isClickable()) {
+                    parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                    break;
+                }
+                parent = parent.getParent();
             }
-            parent = parent.getParent();
         }
-
     }
 
     /**
      * 递归查找当前聊天窗口中的红包信息
-     *
+     * <p>
      * 聊天窗口中的红包都存在"领取红包"一词,因此可根据该词查找红包
      *
      * @param node
@@ -130,14 +143,20 @@ public class RedEnvelopeService extends AccessibilityService {
                     return node;
                 }
             }
+            return null;
         } else {
+            AccessibilityNodeInfo childNode = null;
             for (int i = 0; i < node.getChildCount(); i++) {
                 if (node.getChild(i) != null) {
-                    recycle(node.getChild(i));
+                    childNode = recycle(node.getChild(i));
+                    if (childNode != null) {
+                        break;
+                    }
                 }
             }
+            return node == null ? null : childNode;
+
         }
-        return node;
     }
 
     @Override
@@ -147,8 +166,28 @@ public class RedEnvelopeService extends AccessibilityService {
 
     @Override
     protected void onServiceConnected() {
+        // 在API11之后构建Notification的方式
+        Notification.Builder builder = new Notification.Builder(this.getApplicationContext()); //获取一个Notification构造器
+        Intent nfIntent = new Intent(this, MainActivity.class);
+        builder.setContentIntent(PendingIntent.getActivity(this, 0, nfIntent, 0)) // 设置PendingIntent
+                .setLargeIcon(BitmapFactory.decodeResource(this.getResources(),
+                        R.mipmap.ic_launcher_round)) // 设置下拉列表中的图标(大图标)
+                .setContentTitle("自动抢红包服务") // 设置下拉列表里的标题
+                .setSmallIcon(R.mipmap.ic_launcher_round) // 设置状态栏内的小图标
+                .setContentText("自动抢红包服务替你将红包自动收入囊中！") // 设置上下文内容
+                .setWhen(System.currentTimeMillis()); // 设置该通知发生的时间
+
+        Notification notification = builder.build(); // 获取构建好的Notification
+        notification.defaults = Notification.DEFAULT_SOUND; //设置为默认的声音
+        // 参数一：唯一的通知标识；参数二：通知消息。
+        startForeground(110, notification);// 开始前台服务
         super.onServiceConnected();
     }
 
+    @Override
+    public void onDestroy() {
+        stopForeground(true);// 停止前台服务--参数：表示是否移除之前的通知
+        super.onDestroy();
+    }
 
 }
